79f25334e107981197dd0b939ed98f4479d24f07
Anastasiya Paliashuk <anastasiyapaliashuk@gmail.com>
2023-12-13 15:23:00 +0300

    feat: add getBooks  endpoint

--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 const getHealthStatus = require("../routes/healthcheck/healthcheck");
 const loginUser = require("../routes/loginUser/loginUser");
 const getCategories = require("../routes/getCategories/getCategories");
+const getBooks = require("../routes/getBooks/getBooks");
 
 const router = (app) => {
   app.get("/healthcheck", (request, response) => {
@@ -9,6 +10,10 @@ const router = (app) => {
   });
   app.get("/categories", (request, response) => {
     const responseData = getCategories(request.headers.authorization, response);
+    response.send(responseData);
+  });
+  app.get("/books", (request, response) => {
+    const responseData = getBooks(request.headers.authorization, response);
     response.send(responseData);
   });
   app.get("/", (request, response) => {
